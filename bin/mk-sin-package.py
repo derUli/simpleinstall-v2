8f36499eae4d7910d6babe42c1ed3c7e4542ecf8
Ulrich Schmidt <ulrich1992@web.de>
2016-07-27 18:46:50 +0200

    erster commit

---
+++ bin/mk-sin-package.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+import tarfile
+import os
+import shutil
+import sys
+import json
+import base64
+import hashlib
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+
+def sha1OfFile(filepath):
+    with open(filepath, 'rb') as f:
+        return hashlib.sha1(f.read()).hexdigest()
+
+current_dir = os.getcwd()
+build_json = os.path.join(current_dir, "build.json")
+license_file = os.path.join(current_dir, "license.txt")
+folder_src = os.path.join(current_dir, "src")
+work_dir = os.path.join(current_dir, "work")
+tar_gz_file = os.path.join(work_dir, "package.tar.gz")
+
+if not os.path.exists(build_json):
+    print("build.json not found!")
+    sys.exit(1)
+
+if not os.path.exists(folder_src):
+    print("src folder not found!")
+    sys.exit(2)
+if os.path.exists(work_dir):
+    shutil.rmtree(work_dir)
+
+if not os.path.exists(work_dir):
+    os.makedirs(work_dir)
+
+json_data = open(build_json).read()
+mjson = json.loads(json_data)
+os.chdir("src")
+pkgContent = []
+print("get content list of " + mjson["id"] + "...")
+for root, dirs, files in os.walk(folder_src):
+    for filename in files:
+        p = os.path.join(root, filename)
+        p = p.replace(".\\", "")
+        p = p.replace("./", "")
+        pkgContent.append(p)
+tar = tarfile.open(tar_gz_file, 'w:gz')
+for f in pkgContent:
+    print("Adding " + f + "...")
+    tar.add(f)
+os.chdir("..")
+tar.close()
+checksum = sha1OfFile(tar_gz_file)
+data = base64.b64encode(open(tar_gz_file).read())
+mjson["data"] = data
+mjson["checksum"] = checksum
+
+if os.path.exists(license_file):
+    mjson["license"] = base64.b64encode(open(license_file).read())
+
+print json.dumps(mjson)
