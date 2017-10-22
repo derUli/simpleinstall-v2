66d81020c1c893bf97288e5a6aa37175f24f17fa
Ulrich Schmidt <ulrich1992@web.de>
2017-10-22 15:30:23 +0200

    Script count lines added

---
+++ bin/count-lines.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Lines of php Code:"
+find . -name "*.php"|xargs cat|wc -l
+echo "Lines of js Code:"
+find . -name "*.css"|xargs cat|wc -l
+echo "Lines of css Code:"
+find . -name "*.css"|xargs cat|wc -l
+echo "Lines of py Code:"
+find . -name "*.py"|xargs cat|wc -l
