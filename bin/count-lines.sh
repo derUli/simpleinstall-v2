925392c84c295cf1866e8537617531dcdcabdf61
Ulrich Schmidt <ulrich1992@web.de>
2017-10-22 15:35:04 +0200

    jas

--- bin/count-lines.sh
+++ bin/count-lines.sh
@@ -2,7 +2,7 @@
 echo "Lines of php Code:"
 find . -name "*.php"|xargs cat|wc -l
 echo "Lines of js Code:"
-find . -name "*.css"|xargs cat|wc -l
+find . -name "*.js"|xargs cat|wc -l
 echo "Lines of css Code:"
 find . -name "*.css"|xargs cat|wc -l
 echo "Lines of py Code:"
