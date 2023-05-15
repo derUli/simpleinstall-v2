ad8bacadb1484582aef880d7e795f629fdab0b45
Ulrich Schmidt <support@ulicms.de>
2023-05-15 19:00:17 +0200

    format mk-sin-package.py

--- bin/mk-sin-package.py
+++ bin/mk-sin-package.py
@@ -8,14 +8,16 @@ import base64
 import hashlib
 import time
 
+
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
 def sha1OfFile(filepath):
-    with open(filepath, 'rb') as f:
+    with open(filepath, "rb") as f:
         return hashlib.sha1(f.read()).hexdigest()
+
 
 current_dir = os.getcwd()
 build_json = os.path.join(current_dir, "build.json")
@@ -33,7 +35,7 @@ if not os.path.exists(build_json):
 if not os.path.exists(folder_src):
     print("src folder not found!")
     sys.exit(2)
-    
+
 if os.path.exists(work_dir):
     shutil.rmtree(work_dir)
 
@@ -51,7 +53,7 @@ for root, dirs, files in os.walk("."):
         p = p.replace(".\\", "")
         p = p.replace("./", "")
         pkgContent.append(p)
-tar = tarfile.open(tar_gz_file, 'w:gz')
+tar = tarfile.open(tar_gz_file, "w:gz")
 for f in pkgContent:
     print("Adding " + f + "...")
     tar.add(f)
@@ -59,7 +61,7 @@ os.chdir("..")
 tar.close()
 checksum = sha1OfFile(tar_gz_file)
 data = base64.b64encode(open(tar_gz_file, "rb").read())
-mjson["data"] = data.decode('utf-8')
+mjson["data"] = data.decode("utf-8")
 mjson["checksum"] = checksum
 
 if os.path.exists(license_file):
@@ -69,15 +71,15 @@ screenshot1 = os.path.join(current_dir, "screenshot.jpg")
 screenshot2 = os.path.join(current_dir, "screenshot.png")
 
 if os.path.exists(screenshot1):
-	base_uri = b"image/jpeg;base64," + base64.b64encode(open(screenshot1, "rb").read())
-	mjson["screenshot"] = base_uri.decode('utf-8')
+    base_uri = b"image/jpeg;base64," + base64.b64encode(open(screenshot1, "rb").read())
+    mjson["screenshot"] = base_uri.decode("utf-8")
 elif os.path.exists(screenshot2):
-	base_uri = b"image/png;base64," + base64.b64encode(open(screenshot1, "rb").read())
-	mjson["screenshot"] = base_uri.decode('utf-8')
+    base_uri = b"image/png;base64," + base64.b64encode(open(screenshot1, "rb").read())
+    mjson["screenshot"] = base_uri.decode("utf-8")
 
 if os.path.exists(license_file):
     mjson["license"] = open(license_file).read().strip()
-    
+
 if os.path.exists(description_file):
     mjson["description"] = open(description_file).read().strip()
 
@@ -89,4 +91,4 @@ if not os.path.exists(folder_dist):
 output_file = os.path.join(folder_dist, mjson["id"] + "-" + mjson["version"] + ".sin")
 
 with open(output_file, "w") as handle:
-    handle.write(json.dumps(mjson, ensure_ascii = False))
+    handle.write(json.dumps(mjson, ensure_ascii=False))
